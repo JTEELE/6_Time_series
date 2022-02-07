b37150b7b545006f12a0ce2442d4a4445bec22ed
JTEELE <j.teele@outlook.com>
2022-02-06 20:04:09 -0600

    updated 'regression analysis' and 'functions' file

--- _functions.py
+++ _functions.py
@@ -5,6 +5,16 @@ from sklearn.metrics import mean_squared_error
 from sklearn.linear_model import LinearRegression
 
 
+def LR_model(X_train, Y_train, X_test, Y_test):
+    model = LinearRegression()
+    model.fit(X_train, Y_train)
+    out_sample = Y_test.to_frame()
+    out_sample["Predicted_return"] = model.predict(X_test)
+    in_sample = Y_train.to_frame()
+    in_sample["Predicted_return"] = model.predict(X_train)
+    return in_sample, out_sample
+
+
 def train_test(time_series):
     time_series['Lagged_returns'] = time_series.Returns.shift()
     time_series.dropna(inplace=True)
@@ -16,11 +26,16 @@ def train_test(time_series):
     Y_test = test.Returns.dropna()
     return X_train, X_test, Y_train, Y_test
 
-
-def rmse(Results):
-    mse = mean_squared_error(
-        Results['Returns'],
-        Results['Predicted_return'])
-    out_of_sample_rmse = np.sqrt(mse)
-    print(f'Out of sample root mean squared error is {out_of_sample_rmse}')
-    return out_of_sample_rmse
+# mean_squared_error (MSE) & root-mean-squared error (RMSE, i.e. sqrt of MSE)
+def rmse(in_sample, out_sample):
+    in_rmse = mean_squared_error(
+        in_sample['Returns'],
+        in_sample['Predicted_return'])
+    in_sample_rmse = np.sqrt(in_rmse)
+    print(f'In-sample root mean squared error is {in_sample_rmse}')
+    out_rmse = mean_squared_error(
+        out_sample['Returns'],
+        out_sample['Predicted_return'])
+    out_of_sample_rmse = np.sqrt(out_rmse)
+    print(f'Out-of-sample root mean squared error is {out_of_sample_rmse}')
+    return in_rmse, out_rmse
